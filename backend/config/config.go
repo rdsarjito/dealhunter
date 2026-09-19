2ae16a424f5763e31ec30117ed0edf6f3db30769
rdsarjito <rama.dhaninursarjito@gmail.com>
2026-09-19 20:15:22 +0700

    config: add MinIO S3 configuration fields
    - Add MinioEndpoint, MinioAccessKey, MinioSecretKey, MinioBucket,
      MinioPublicURL, MinioUseSSL fields to Config struct
    - Load all MinIO env vars with sensible local defaults
    - Update .env.example with MinIO configuration template
    - Run go mod tidy: add minio-go/v7 and updated dependencies

--- backend/config/config.go
+++ backend/config/config.go
@@ -17,6 +17,14 @@ type Config struct {
 	DBName           string
 	DBSSLMode        string
 	TelegramBotToken string
+
+	// MinIO / S3 object storage
+	MinioEndpoint  string
+	MinioAccessKey string
+	MinioSecretKey string
+	MinioBucket    string
+	MinioPublicURL string // public base URL for serving files
+	MinioUseSSL    bool
 }
 
 func LoadConfig() *Config {
@@ -61,7 +69,31 @@ func LoadConfig() *Config {
 
 	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
 
-	log.Printf("[Config] Loaded environment for port: %s, DB: %s@%s:%s/%s", port, dbUser, dbHost, dbPort, dbName)
+	// MinIO config
+	minioEndpoint := os.Getenv("MINIO_ENDPOINT")
+	if minioEndpoint == "" {
+		minioEndpoint = "localhost:9000"
+	}
+	minioAccessKey := os.Getenv("MINIO_ACCESS_KEY")
+	if minioAccessKey == "" {
+		minioAccessKey = "dealhunter"
+	}
+	minioSecretKey := os.Getenv("MINIO_SECRET_KEY")
+	if minioSecretKey == "" {
+		minioSecretKey = "dealhunter_minio_secret"
+	}
+	minioBucket := os.Getenv("MINIO_BUCKET")
+	if minioBucket == "" {
+		minioBucket = "dealhunter"
+	}
+	minioPublicURL := os.Getenv("MINIO_PUBLIC_URL")
+	if minioPublicURL == "" {
+		minioPublicURL = "http://localhost:9000/dealhunter"
+	}
+	minioUseSSL := os.Getenv("MINIO_USE_SSL") == "true"
+
+	log.Printf("[Config] Loaded: port=%s DB=%s@%s:%s/%s MinIO=%s bucket=%s",
+		port, dbUser, dbHost, dbPort, dbName, minioEndpoint, minioBucket)
 
 	return &Config{
 		AppPort:          port,
@@ -73,5 +105,11 @@ func LoadConfig() *Config {
 		DBName:           dbName,
 		DBSSLMode:        dbSSLMode,
 		TelegramBotToken: botToken,
+		MinioEndpoint:    minioEndpoint,
+		MinioAccessKey:   minioAccessKey,
+		MinioSecretKey:   minioSecretKey,
+		MinioBucket:      minioBucket,
+		MinioPublicURL:   minioPublicURL,
+		MinioUseSSL:      minioUseSSL,
 	}
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,6 +1,6 @@
 module github.com/rdsarjito/dealhunter-backend
 
-go 1.24.0
+go 1.25.0
 
 require (
 	github.com/go-rod/rod v0.116.2
@@ -13,17 +13,18 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.2 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/klauspost/compress v1.19.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.23 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
@@ -32,8 +33,8 @@ require (
 	github.com/ysmood/got v0.40.0 // indirect
 	github.com/ysmood/gson v0.7.3 // indirect
 	github.com/ysmood/leakless v0.9.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.29.0 // indirect
+	golang.org/x/crypto v0.55.0 // indirect
+	golang.org/x/sync v0.22.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/text v0.41.0 // indirect
 )
